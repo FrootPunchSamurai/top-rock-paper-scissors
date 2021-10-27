5d9968b47fd45521b9e81e87dc3e18c081d97b64
Amruth Kiran <amruthkiran47@gmail.com>
2021-10-27 09:25:15 +0530

    Updated js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ function userChoice() {
     return userChoice
   }
 
+
 function computerChoice() {
     let computerChoice = (Math.random()*3)
 
@@ -19,32 +20,85 @@ function computerChoice() {
 
 function playRound(playerSelection, computerSelection){
     if(playerSelection == computerSelection){
-        console.log("Draw , no one wins");
+        // console.log("Draw , no one wins");
+        return -1;
     }
 
     if(playerSelection == "rock" && computerSelection == "scissors"){
-        console.log("You Lose, you chose rock computer chose scissors");
+        // console.log("You Lose, you chose rock computer chose scissors");
+        return 0;
     }
     else if(playerSelection == "rock" && computerSelection == "paper"){
-        console.log("You win , you chose rock computer chose paper");
+        // console.log("You win , you chose rock computer chose paper");
+        return 1;
+
     }
 
     if(playerSelection == "paper" && computerSelection == "scissors"){
-        console.log("You Lose, you chose paper computer chose scissors");
+        // console.log("You Lose, you chose paper computer chose scissors");
+        return 0;
+
     }
     else if(playerSelection == "paper" && computerSelection == "rock"){
-        console.log("You win , you chose paper computer chose rock");
+        // console.log("You win , you chose paper computer chose rock");
+        return 1;
+
     }
 
     if(playerSelection == "scissors" && computerSelection == "paper"){
-        console.log("You win , you chose scissors computer chose paper");
+        // console.log("You win , you chose scissors computer chose paper");
+        return 1;
+
     }
     else if(playerSelection == "scissors" && computerSelection == "rock"){
-        console.log("You Lose , you chose scissors computer chose rock");
+        // console.log("You Lose , you chose scissors computer chose rock");
+        return 0;
+
     }
 }
 
-let playerSelection = userChoice();
-let computerSelection = computerChoice();
+function game(){
 
-playRound(playerSelection, computerSelection);
+    let cscore = 0, pscore = 0;
+
+    for (let i = 1; i <= 5; i++) {
+
+        let playerSelection = userChoice();
+        let computerSelection = computerChoice();
+        let status = playRound(playerSelection, computerSelection);
+
+        if(status == -1){
+            console.log("Draw , no one wins");
+        }
+
+        if(status == 0){
+            console.log("You Lost this round");
+            cscore++;
+        }
+
+        if(status == 1){
+            console.log("You won this round");
+            pscore++;
+        }
+
+    }
+
+    console.log("You Scored " + pscore + " Computer Scored " + cscore);
+
+    if(pscore > cscore){
+        console.log("You win");
+    }
+
+    else if(pscore == cscore){
+        console.log("Draw");
+    }
+
+    else{
+        console.log("You Lose");
+    }
+
+}
+
+game();
+
+
